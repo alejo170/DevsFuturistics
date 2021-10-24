284d282a2251a69fd4557d7c238ff8a8748b83a0
Alejandro <alejo170@gmail.com>
2021-10-24 12:29:40 -0500

    Probando

--- src/paginas/admin/Productos.jsx
+++ src/paginas/admin/Productos.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import url from '../../componentes/constantes/urlback';
+import url from '../../componentes/constantes/urlBack';
 
 const Productos = () => {
 
